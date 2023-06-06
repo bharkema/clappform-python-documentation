2018d0e8fc50873b338569050aeb6388ef374c27
Wybren Terpstra <wterpstra@protonmail.com>
2023-06-06 14:23:07 +0200

    Changed version to 3.0.0-alpha.2

--- src/clappform/__init__.py
+++ src/clappform/__init__.py
@@ -31,7 +31,7 @@ from .exceptions import (
 
 
 # Metadata
-__version__ = "3.0.0-alpha.1"
+__version__ = "3.0.0-alpha.2"
 __author__ = "Clappform B.V."
 __email__ = "info@clappform.com"
 __license__ = "MIT"
